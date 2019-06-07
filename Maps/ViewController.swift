c268f93b40b6be7b763b99d6e29deddc34368663
marek <marek@macbook-pro-marek.home>
2019-06-07 11:31:08 +0200

    Added simple UI with Polylines

--- Maps/ViewController.swift
+++ Maps/ViewController.swift
@@ -7,14 +7,51 @@
 //
 
 import UIKit
+import MapKit
+class ViewController: UIViewController, MKMapViewDelegate {
 
-class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var myInitLocation = CLLocationCoordinate2D(latitude: 54.3520, longitude: 18.6466)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let span = MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+        let region = MKCoordinateRegion(center: myInitLocation, span: span)
+        
+        mapView.setRegion(region, animated: true)
+        mapView.delegate = self
+        
+        createPolyline()
     }
 
+    func createPolyline() {
+        let locations = [CLLocationCoordinate2D(latitude: 54.3520, longitude: 18.6466),
+                         CLLocationCoordinate2D(latitude: 54.3600, longitude: 18.6366),
+                         CLLocationCoordinate2D(latitude: 54.3640, longitude: 18.6266),
+                         CLLocationCoordinate2D(latitude: 54.3660, longitude: 18.6166)]
+        let aPolyline = MKPolyline(coordinates: locations, count: locations.count)
+        mapView.addOverlay(aPolyline)
+    }
 
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if (overlay is MKPolyline) {
+            let polylineRender = MKPolylineRenderer(overlay: overlay)
+            polylineRender.strokeColor = UIColor.red.withAlphaComponent(0.5)
+            polylineRender.lineWidth = 5
+            return polylineRender
+        }
+        return MKPolylineRenderer()
+    }
+
+    
+    @IBAction func selectRoadButtonClicked(_ sender: Any) {
+        
+        
+    }
+    
 }
 
